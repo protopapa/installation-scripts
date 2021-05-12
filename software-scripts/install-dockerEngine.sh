d0a93c7a64197f5af6d09b359dd5772abe42c7b1
protopapa <natassa.prt@gmail.com>
2021-05-12 17:22:16 +0200

    some changes I do not remember

--- software-scripts/install-dockerEngine.sh
+++ software-scripts/install-dockerEngine.sh
@@ -21,12 +21,12 @@ sudo apt-get install \
     gnupg-agent \
     software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88
 
 sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
    $(lsb_release -cs) \
    stable"
 
@@ -38,4 +38,4 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 
 sudo groupadd docker
-sudo usermod -aG docker protopapa
+sudo usermod -aG docker $USER
