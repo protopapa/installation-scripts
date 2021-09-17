b38b13cdcf853013c0d85329342a5f20b8e39e43
Anastasia Protopapa <anastasia@wgtwo.com>
2021-09-17 18:34:16 +0200

    minor changes for the core-software

--- install-core-software.sh
+++ install-core-software.sh
@@ -7,9 +7,7 @@
 #
 #   DESCRIPTION: Script to install utility/entertainment software
 #
-#        AUTHOR: Anastasia Protopapa,
-#       CREATED: 2020-04-05 16:48:05
-#      REVISION:  001
+#        AUTHOR: Anastasia Protopapa
 #===============================================================================
 
 
@@ -21,11 +19,6 @@ sudo apt-get update;
 # VLC Media Player
 
 sudo apt-get install vlc
-
-# Skype
-
-wget https://go.skype.com/skypeforlinux-64.deb -P ~/Downloads
-sudo apt install /home/${user}/Downloads/skypeforlinux-64.deb
 
 # Atom
 sh software-scripts/install-atom.sh
