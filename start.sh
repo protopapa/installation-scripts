4603f0aa97688ba8c16311f2e6ee648c4dd75b0b
Anastasia Protopapa <anastasia.protopapa@protonmail.com>
2024-03-15 11:33:08 +0200

    from bash to ansible

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+apt update
+
+apt-get install python3 python3-pip python3-venv
+python3 -m venv .venv
+
+source .venv/bin/activate
+if [[ "$VIRTUAL_ENV" == "" ]]
+then
+  echo "Python venv failed in activate. Exiting"
+  exit 0
+fi
+
+python3 -m pip install --upgrade pip
+python3 -m pip install  ansible
+
+ansible-playbook local-playbook.yaml
